ed4686921d7a1ca724d3655bb49c8da7ece39f20
Antoine Fabri <antoine.fabri@gmail.com>
2024-07-02 17:57:22 +0200

    simplify the package

--- R/constructive.example-package.R
+++
@@ -1,9 +0,0 @@
-#' @importFrom constructive .cstr_apply .cstr_combine_errors .cstr_construct
-#'   .cstr_options .cstr_pipe .cstr_repair_attributes .cstr_wrap
-#' @importFrom utils getS3method
-#' @keywords internal
-"_PACKAGE"
-
-## usethis namespace: start
-## usethis namespace: end
-NULL
--- R/integer.R
+++
@@ -1,8 +0,0 @@
-#' @export
-.cstr_construct.integer.ones <- function(x, ...) {
-  opts <- list(...)$opts$integer %||% opts_integer()
-  curly <- opts$curly %||% FALSE
-  code <- paste(rep("1L", x), collapse = " + ")
-  code <- if (curly) sprintf("{%s}", code) else sprintf("(%s)", code)
-  .cstr_repair_attributes(x, code, ...)
-}
